fd56651bd9e8ddd12cd56374da375912b24eafeb
Dylan Rodriguez <dylanrod24@users.noreply.github.com>
2018-10-31 22:12:29 -0700

    removed forecast.io

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
-var io = require('forecast.io.live')(Server, '9f5c21e8b5dbe91f6f570d294de8a42b', [60000] )
+var server = app.listen(4000, function(){
+    console.log("running on port 4000");
+});
+var socketIo = require('socket.io')(server);
+var axios = require('axios');
+
+var getApiAndEmit = "TODO";
 
 app.use(express.json());
 app.use(express.static(__dirname + '/public/dist/public'));
 
 require('./server/config/routes.js')(app);
 
-Server.listen(4000, function(){
-    console.log("running on port 4000");
-})
+
